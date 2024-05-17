7ea4343b6d89c7a2c1ddd6aa4c030331bb6fc52b
Anand2510 <159130532+Anand2510@users.noreply.github.com>
2024-05-17 11:01:11 +0530

    save

--- assets/application.js
+++ assets/application.js
@@ -1,2 +1,8 @@
 // Put your application javascript here
 // alert("This page working || not working");
+document.querySelector('sort_by').addEventListener('change',function(e){
+    var url = new URL(window.location.href);
+    url.searchParams.set('sort_by', e.currentTarget.value);
+
+    window.location = url.href;
+});
