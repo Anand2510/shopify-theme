21a817ba2a87f962a6514528ec2ed4c50702ed63
Anand2510 <159130532+Anand2510@users.noreply.github.com>
2024-05-17 11:03:06 +0530

    save

--- assets/application.js
+++ assets/application.js
@@ -1,6 +1,6 @@
 // Put your application javascript here
 // alert("This page working || not working");
-document.querySelector('sort_by').addEventListener('change',function(e){
+document.querySelector('#sort_by').addEventListener('change',function(e){
     var url = new URL(window.location.href);
     url.searchParams.set('sort_by', e.currentTarget.value);
 
