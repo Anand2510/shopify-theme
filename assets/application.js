30112dccf82811fd97d8236a8c21a7ebbc4ddf66
Anand <ananda@rvsmedia.com>
2024-10-10 18:09:57 +0530

    save

--- assets/application.js
+++ assets/application.js
@@ -10,7 +10,7 @@ document.querySelector('#sort_by').addEventListener('change', function(e) {
 
 if( document.getElementById('AddressCountryNew') != null){
     document.getElementById('AddressCountryNew').addEventListener('change', function(e){
-            var province = this.options[selectedIndex].getAttribute('data-provinces');
+            var province = this.options[this.selectedIndex].getAttribute('data-provinces');
             var proviceSelector = document.getElementById('AddressCountryNew');
             var provinceArray = JSON.parse(province);
 
